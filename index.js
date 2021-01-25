e68209518bf22a5243e45a3e0dc4c00d640582a9
lindellcarternyc <lindellcarternyc@gmail.com>
2021-01-25 14:46:20 -0500

    complete task 1

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+      counter1 makes use of a private variable to keep track of state, whereas counter2 acceses a global variable
   2. Which of the two uses a closure? How can you tell?
-  
+      coutner1 makes use of a closure. You can tell because the returned function access it's parent's scope  
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
